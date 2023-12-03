023771e24e68c85016ca44fa78b3d9bfc81c5ef6
singhprabhjot16 <137317501+singhprabhjot16@users.noreply.github.com>
2023-12-03 13:52:36 +0530

    Update ProjectFile.py

--- ProjectFile.py
+++ ProjectFile.py
@@ -95,6 +95,7 @@ class MoverHandler(FileSystemEventHandler):
                 logging.info(f"Moved document file: {name}")
 
 if __name__ == "__main__":
+    print("\nAutomating your download process...\n")
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
